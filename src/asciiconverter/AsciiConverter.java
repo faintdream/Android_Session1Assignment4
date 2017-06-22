9c68bd6c5442e11da487309bbe3e870b4a01d056
faintdream <dubey.akash@gmail.com>
2017-06-22 18:20:00 +0530

    Program convert int to ascii char from A to Z, in tabular format

--- src/asciiconverter/AsciiConverter.java
+++ src/asciiconverter/AsciiConverter.java
@@ -10,11 +10,13 @@ public class AsciiConverter {
     
      void convertToAscii(){ 
          //loop with run from 69 to 90 or we can say A to Z (26 times)
-         while (input < 90) 
+         while (input <= 90) 
          {
-             result=(char)input; //typecasting integer to char
-             System.out.println(result); // display char
-             input++;
+             result=(char)input; //typecasting integer to char             
+             System.out.print(input+"-"+result+"\t"); // put a tab space
+             if ( input%8 ==0) //everytime remainder is 0 put a next line using 
+                System.out.println("");// println                 
+             input++;             //increment input
          }
      }
      
