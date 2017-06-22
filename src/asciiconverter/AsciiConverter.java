70dec014030336936e1780392b5a9570919c3cc0
faintdream <dubey.akash@gmail.com>
2017-06-22 15:49:10 +0530

    Program convert int to ascii char from A to Z

--- src/asciiconverter/AsciiConverter.java
+++ src/asciiconverter/AsciiConverter.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package asciiconverter;
 
-/**
- *
- * @author FLAdmin
- */
 public class AsciiConverter {
 
-    /**
-     * @param args the command line arguments
-     */
+    /* Program to convert int to ascii, we will simply typecase to get the answer
+    
+    */
+     int input=65; // we begin with65 which also represents ascii 'A'
+     char result;
+    
+     void convertToAscii(){ 
+         //loop with run from 69 to 90 or we can say A to Z (26 times)
+         while (input < 90) 
+         {
+             result=(char)input; //typecasting integer to char
+             System.out.println(result); // display char
+             input++;
+         }
+     }
+     
+     
     public static void main(String[] args) {
-        // TODO code application logic here
+        AsciiConverter obj =new AsciiConverter();
+        obj.convertToAscii();
     }
+
+    
     
 }
